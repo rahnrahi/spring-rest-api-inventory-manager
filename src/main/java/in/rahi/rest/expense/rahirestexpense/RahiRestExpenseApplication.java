fcc5be480ac9e3f5b7b8929531777e1e62e46f4c
Rahi <rahnrahi@gmail.com>
2023-10-29 15:18:13 +0530

    Project initialized with boilerplate

---
+++ src/main/java/in/rahi/rest/expense/rahirestexpense/RahiRestExpenseApplication.java
@@ -0,0 +1,13 @@
+package in.rahi.rest.expense.rahirestexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahiRestExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RahiRestExpenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/rahi/rest/expense/rahirestexpense/RahiRestExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package in.rahi.rest.expense.rahirestexpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RahiRestExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
